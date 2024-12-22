f9a49a484e7921f13314907c77a21988f56d6372
Ghulam Abbas <g3165374@gmail.com>
2024-12-22 12:10:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail="abbas@gmail.com"
+var accountPassword="12345"
+accountCity="Rawalpindi"
+
+// accountId=5// Not Allowed
+console.log(accountId)
+accountEmail="example@gmail.com"
+accountPassword="234231"
+accountCity="Pindigheb"
+console.table([accountId , accountEmail , accountPassword , accountCity])
